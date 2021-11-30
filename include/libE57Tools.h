29643b748bb3d84f7e9517cfeba95f71450ca007
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-11-30 20:50:32 +0100

    Ooops

--- include/libE57Tools.h
+++ include/libE57Tools.h
@@ -5,6 +5,14 @@ typedef struct {
 	int64_t cartesianY;
 	int64_t cartesianZ;
 	int64_t cartesianInvalidState;
+	int64_t intensity;
+	int64_t colorRed;
+	int64_t colorGreen;
+	int64_t colorBlue;
+	int64_t columnIndex;
+	int64_t rowIndex;
+	int64_t BuffersFloatsCount;
+	int64_t BuffersIntsCount;
 
 } ToolsFileInfo;
 
